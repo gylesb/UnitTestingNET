121c850cf95f3d09b5d45bf6850841aa6d41b8f6
Gyles Batara <gyles.batara@live.com>
2018-01-03 16:15:11 -0800

    Mess around with components

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Linq;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            ToDoList<Item> model = db.Items.ToList();
+            return View(model);
+        }
+
+		public IActionResult Details(int id)
+		{
+			Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+			return View(thisItem);
+		}
+    }
+}
---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models 
+{
+    public class ToDoListContext : DbContext
+    {
+        public DbSet<items> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+    }
+}
